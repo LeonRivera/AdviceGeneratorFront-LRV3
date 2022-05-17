a9466c53929d3adbd282903de2ae19539f1d96c0
LeonRivera <leonriv2@gmail.com>
2022-05-16 22:54:32 -0500

    16052022 | LeonRivera | Some files for design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AdviceComponent } from './advice/advice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AdviceComponent
   ],
   imports: [
     BrowserModule
